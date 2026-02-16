589d638d45365e8c6d88501a86c9299b97cbf9f0
CONNER QUINN <ConnerQ@connerscloud.com>
2026-02-16 17:29:19 -0600

    First main changes

---
+++ terraform-configs/main.tf
@@ -0,0 +1,19 @@
+terraform{
+    minikube = {
+        source = "scott-the-programmer/minikube"
+        version = "0.42"
+    }
+}
+
+provider "minikube" {
+    kubernetes_version = "1.30"
+}
+
+resource "minikube_cluster" "minikube_docker" {
+    driver = "docker"
+    cluster_name = "complete-devops-project"
+    addons = [
+        "default-storageclass",
+        "storage-provisioner"
+    ]
+}
