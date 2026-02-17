4cf15233032ea3c9d9b39b96eb0af59a744b010f
CONNER QUINN <ConnerQ@connerscloud.com>
2026-02-17 12:46:16 -0600

    Add CI workflow, Dockerfile, and update Terraform configs

--- terraform-configs/main.tf
+++ terraform-configs/main.tf
@@ -2,13 +2,13 @@ terraform{
     required_providers {
          minikube = {
             source = "scott-the-programmer/minikube"
-            version = "0.4.2"
+            version = "0.6.0"
         }
     }
 }
 
 provider "minikube" {
-    kubernetes_version = "1.30"
+    kubernetes_version = "v1.35.0"
 }
 
 resource "minikube_cluster" "minikube_docker" {
